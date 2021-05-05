ab39e725ab3f4f41fd4ea3a7628bb72ff7a2e5ec
Youssef Zakaria <youssef.zakaria.dev@gmail.com>
2021-05-05 03:23:11 +0200

    Branching Resolved

--- android_flickr/lib/main.dart
+++ android_flickr/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: FlickrCameraScreen(),
+      home: GetStartedScreen(),
     );
   }
 }
