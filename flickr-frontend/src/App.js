4da17edc085689c95e7741aaf7043f4232167da9
SarahOssama <62894826+SarahOssama@users.noreply.github.com>
2021-05-07 01:34:51 +0200

    fixing issues in Photo Component , adding Routing

--- flickr-frontend/src/App.js
+++ flickr-frontend/src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
 // import Addv from './Components/Addv';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Route, Link, Switch,
+} from 'react-router-dom';
 import Home from './Components/HomePage/Home';
 import NavBar from './Components/NavBar/NavBar';
+import About from './Components/NavBar/About';
+
 // import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <Router>
-
       <div className="App">
         <NavBar />
-        {/* <Addv /> */}
-        <div className="OuterFeedContainer">
-          <Home />
-        </div>
+        <Switch>
+          <Route path="/Home" exact component={Home} />
+
+          <Route path="/about" component={About} />
+        </Switch>
       </div>
     </Router>
 
