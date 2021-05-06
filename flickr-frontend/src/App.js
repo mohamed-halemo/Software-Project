ecd5260c497b7df37d45181b2dc624f791dc4ef8
SarahOssama <62894826+SarahOssama@users.noreply.github.com>
2021-05-06 20:34:25 +0200

    basic Home Page after JSon REquest

--- flickr-frontend/src/App.js
+++ flickr-frontend/src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
 // import Addv from './Components/Addv';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Components/HomePage/Home';
 import NavBar from './Components/NavBar/NavBar';
 // import Button from "react-bootstrap/Button";
 
 function App() {
   return (
+    <Router>
 
-    <div className="App">
-      <NavBar />
-      {/* <Addv /> */}
-      <div className="content">
-        <Home />
+      <div className="App">
+        <NavBar />
+        {/* <Addv /> */}
+        <div className="OuterFeedContainer">
+          <Home />
+        </div>
       </div>
-    </div>
+    </Router>
 
   );
 }
