f32facd2f8d98189d730cc6f987483d38d9c43e9
mennat allah nawar <55787939+mennatallah-nawar@users.noreply.github.com>
2021-05-07 00:49:48 +0200

    SignUp commit

--- flickr-frontend/src/App.js
+++ flickr-frontend/src/App.js
@@ -1,19 +1,27 @@
-import './App.css';
+// import { router } from 'json-server';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import StartPage from './Components/StartPage/StartPage';
+import FormSignup from './Components/StartPage/SingUP/FormSignup';
+// import './App.css';
 // import Addv from './Components/Addv';
-import Home from './Components/HomePage/Home';
-import NavBar from './Components/NavBar/NavBar';
+// import Home from './Components/HomePage/Home';
+// import NavBar from './Components/NavBar/NavBar';
 // import Button from "react-bootstrap/Button";
 
 function App() {
   return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <StartPage />
+        </Route>
 
-    <div className="App">
-      <NavBar />
-      {/* <Addv /> */}
-      <div className="content">
-        <Home />
-      </div>
-    </div>
+        <Route path="/SignUp" exact>
+          <FormSignup />
+        </Route>
+      </Switch>
+    </Router>
 
   );
 }
