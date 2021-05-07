ad61b92850bd4a228ee425bdde003ad3ab8aec6e
SarahOssama <62894826+SarahOssama@users.noreply.github.com>
2021-05-07 21:12:33 +0200

    Fix Home

--- flickr-frontend/src/App.js
+++ flickr-frontend/src/App.js
@@ -1,12 +1,12 @@
 // import { router } from 'json-server';
 import React from 'react';
-import StartPage from './Components/StartPage/StartPage';
-import FormSignup from './Components/StartPage/SingUP/FormSignup';
-// import './App.css';
-// import Addv from './Components/Addv';
 import {
   BrowserRouter as Router, Route, Link, Switch,
 } from 'react-router-dom';
+// import StartPage from './Components/StartPage/StartPage';
+// import FormSignup from './Components/StartPage/SingUP/FormSignup';
+// import './App.css';
+// import Addv from './Components/Addv';
 import Home from './Components/HomePage/Home';
 import NavBar from './Components/NavBar/NavBar';
 import About from './Components/NavBar/About';
@@ -22,18 +22,17 @@ function App() {
         <NavBar />
         <Switch>
           <Route path="/Home" exact component={Home} />
-        <Route path="/" exact>
-          <StartPage />
-        </Route>
+          {/* <Route path="/" exact>
+            <StartPage />
+          </Route>
 
-        <Route path="/SignUp" exact>
-          <FormSignup />
-        </Route>
+          <Route path="/SignUp" exact>
+            <FormSignup />
+          </Route> */}
           <Route path="/about" component={About} />
         </Switch>
       </div>
     </Router>
-
 
   );
 }
