12dc9d587fca86551f8f3ff114a15940ca268e3e
Moaz Elsherbini <moaaz.elsherbini00@eng-st.cu.edu.eg>
2021-05-07 23:29:05 +0200

    Explore(except comments)

---
+++ android_flickr/lib/screens/explore_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import '../widgets/Explore_display.dart';
+import 'package:provider/provider.dart';
+import '../providers/flickr_posts.dart';
+
+class ExploreScreen extends StatelessWidget {
+  static const routeName = '/explore-screen';
+
+  //when the explore is refreshed this function is called to fetch the latest data from the database
+  Future<void> _refreshExplore(BuildContext context) async {
+    await Provider.of<Posts>(context, listen: false).fetchAndSetExplorePosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //this is the Main screen where we have diffrent tabs one of them is only configured which is the explore display
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        backgroundColor: Colors.black.withOpacity(0.5),
+        body: NestedScrollView(
+          //to scroll through posts and also scroll the tab bar
+          headerSliverBuilder: (context, index) {
+            return [
+              SliverAppBar(
+                elevation: 0,
+                toolbarHeight: 0,
+                floating: true,
+                automaticallyImplyLeading: false,
+                backgroundColor: Colors.grey.shade900,
+                bottom: TabBar(
+                  indicatorColor: Colors.transparent,
+                  //isScrollable: true,
+                  tabs: <Widget>[
+                    Tab(
+                      icon: Icon(
+                        Icons.image,
+                      ),
+                      //text: 'Categories',
+                    ),
+                    Tab(
+                      icon: Icon(
+                        Icons.search,
+                      ),
+                      //text: 'Favorites',
+                    ),
+                    Tab(
+                      icon: Icon(
+                        Icons.account_circle,
+                        //color: Colors.white,
+                      ),
+                      //text: 'Categories',
+                    ),
+                    Tab(
+                      icon: Icon(
+                        Icons.notifications,
+                      ),
+                      //text: 'Favorites',
+                    ),
+                    Tab(
+                      icon: Icon(
+                        Icons.camera_alt,
+                      ),
+                      //text: 'Categories',
+                    ),
+                  ],
+                ),
+              )
+            ];
+          },
+          dragStartBehavior: DragStartBehavior.start,
+          body: TabBarView(
+            //widgets each tab will display
+            children: <Widget>[
+              //wrapped with refresh indicator which runs the _refreshExplore function when triggered
+              RefreshIndicator(
+                onRefresh: () {
+                  return _refreshExplore(context);
+                },
+                child: Explore(), //display the explore posts
+              ),
+              Center(
+                child: Text("no search"),
+              ),
+              Center(
+                child: Text("no profile"),
+              ),
+              Center(
+                child: Text("no notifications"),
+              ),
+              Center(
+                child: Text("no camera"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ android_flickr/lib/widgets/explore_post.dart
@@ -0,0 +1,140 @@
+import './few_details_of_faves_comments.dart';
+import 'package:flutter/material.dart';
+import '../providers/flickr_post.dart';
+import 'package:provider/provider.dart';
+import 'pic_postedBy_info_on_post.dart';
+import '../screens/click_on_image_screen.dart';
+
+//explore post describes how the post will be displayed and the widgets it will use
+class ExplorePost extends StatelessWidget {
+  //navigator is used here to navigate to the click_on_image_screen widget when we click on the image
+  void clickOnImageScreen(BuildContext ctx, PostDetails postInformation) {
+    Navigator.of(ctx).pushNamed(
+      ClickOnImageScreen.routeName,
+      arguments: postInformation,
+    );
+  }
+
+  /* we set up a listener here to class Posts using provider.of<PostDetails>(context)*/
+  
+  @override
+  Widget build(BuildContext context) {
+    final postInformation = Provider.of<PostDetails>(context);
+    return ChangeNotifierProvider(
+      create: (context) => postInformation,
+      child: Container(
+        //we return container that includes details of widgets inside the container and their configurations
+        alignment: Alignment.topLeft,
+        decoration: BoxDecoration(color: Colors.white),
+        padding: EdgeInsets.all(5),
+        margin: EdgeInsets.all(8),
+        child: Column(
+          children: <Widget>[
+            /* we wrap the container that contains the posted image usinG with gesture detector so when the user presses on the image,
+              we go to ClickOnImagesscreen widget*/
+            GestureDetector(
+              onTap: () {
+                clickOnImageScreen(context, postInformation);
+              },
+              child: Container(
+                constraints: BoxConstraints(
+                  maxHeight: MediaQuery.of(context).size.height -
+                      MediaQuery.of(context).size.height / 4,
+                ),
+                child: ClipRRect(
+                  child: FadeInImage(
+                    placeholder: AssetImage("assets/images/placeholder.png"),
+                    image: NetworkImage(
+                      postInformation.postImageUrl,
+                    ),
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    /* child: Image.network(
+                      postInformation.postImageUrl,
+                      //height: 250,
+
+                      /* height: MediaQuery.of(context).size.height -
+                            MediaQuery.of(context).size.height / 4, */
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ), */
+                  ),
+                ),
+              ),
+            ),
+            //we provied it with the instaince of PostDetails class that is postImformation so it can display the widgets below post Image
+            PicPostedByInfoOnPost(postInformation),
+            //configurations and widgets choosen for the three buttons, fave , comments and share
+            Container(
+              width: MediaQuery.of(context).size.width / 1.1,
+              child: Divider(
+                thickness: 2,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      IconButton(
+                        icon: postInformation.favesDetails.isFaved
+                            ? Icon(
+                                Icons.star,
+                                color: Colors.blue,
+                              )
+                            : Icon(
+                                Icons.star_border_outlined,
+                              ),
+                        onPressed: () {
+                          postInformation.toggleFavoriteStatus();
+                        },
+                      ),
+                      if (postInformation.favesDetails.favesTotalNumber != 0)
+                        Text(
+                          postInformation.favesDetails.favesTotalNumber
+                              .toString(),
+                        ),
+                    ],
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.comment_outlined),
+                          onPressed: () {},
+                        ),
+                        if (postInformation.commentsTotalNumber != 0)
+                          Text(
+                            postInformation.commentsTotalNumber.toString(),
+                          ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(
+                          Icons.share_outlined,
+                        ),
+                        onPressed: () {},
+                      ),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      //Text("6"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            //few details displayed about the comment(last comment) and one or two names if availabe of the users who faved the post
+            FewDetailsOfFavesComments(postInformation),
+          ],
+        ),
+      ),
+    );
+  }
+}
