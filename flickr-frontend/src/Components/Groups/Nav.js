ec94133bb3f9053f9fd53951dbc844463c31c75f
Menna Elkhodary <Minnaelkhodary89@gmail.com>
2021-05-07 07:13:41 +0200

    GroupOverview
    Menna Group Overview

---
+++ flickr-frontend/src/Components/Groups/Nav.js
@@ -0,0 +1,15 @@
+import './Nav.css';
+
+const Nav = () => (
+  <div>
+    <nav className="Nav">
+      <div className="link">
+        <a href="/">Overview </a>
+        <a href="/">Photos</a>
+        <a href="/">Members</a>
+        <a href="/">Map</a>
+      </div>
+    </nav>
+  </div>
+);
+export default Nav;
