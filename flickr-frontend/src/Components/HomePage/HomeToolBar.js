b86ac2ef64312a381a397d48843231ce446867f0
SarahOssama <62894826+SarahOssama@users.noreply.github.com>
2021-05-08 06:19:42 +0200

    fixes

--- flickr-frontend/src/Components/HomePage/HomeToolBar.js
+++ flickr-frontend/src/Components/HomePage/HomeToolBar.js
@@ -6,19 +6,24 @@ import { IconContext } from 'react-icons';
 const HomeToolBar = () => {
   const handleClick = (e) => {
     // eslint-disable-next-line no-console
-    console.log('All Activity test button', e);
+    console.log('Click', e);
   };
   return (
     <div className="layoutBar">
       <div className="HomeFilterContainer">
         <button type="button" onClick={handleClick}>All Activity</button>
-        <AiFillCaretDown style={{ color: 'rgb(137, 137, 137)', marginLeft: '7px' }} />
+        <AiFillCaretDown
+          style={{
+            color: 'rgb(137, 137, 137)', marginLeft: '7px', marginTop: '4px', height: '15px', width: '13px',
+          }}
+          onClick={handleClick}
+        />
       </div>
       <div className="TabLayoutContainer">
         <IconContext.Provider value={{ className: 'layoutIcons' }}>
-          <BsGrid3X3GapFill />
-          <RiLayoutMasonryFill />
-          <BsSquareFill />
+          <BsGrid3X3GapFill onClick={handleClick} />
+          <RiLayoutMasonryFill onClick={handleClick} />
+          <BsSquareFill onClick={handleClick} />
         </IconContext.Provider>
       </div>
     </div>
