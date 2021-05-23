752bfe6489d1891e632a19cdb1aad468f2ba745c
Moaz Elsherbini <moaaz.elsherbini00@eng-st.cu.edu.eg>
2021-05-23 14:10:33 +0200

    search_people

--- android_flickr/lib/widgets/search_people.dart
+++ android_flickr/lib/widgets/search_people.dart
@@ -1,8 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/flickr_posts.dart';
 
 class SearchPeople extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final peopleSearchDetails = Provider.of<Posts>(context).posts;
+    return ListView.builder(
+      itemCount: peopleSearchDetails.length,
+      itemBuilder: (ctx, index) {
+        return ListTile(
+          leading: CircleAvatar(
+            radius: MediaQuery.of(context).size.width / 20,
+            backgroundImage: NetworkImage(
+              peopleSearchDetails[index].picPoster.profilePicUrl,
+            ),
+            backgroundColor: Colors.transparent,
+            /* child: Image.asset(
+                postInformation.url,
+                alignment: Alignment.center,
+                fit: BoxFit.fill,
+
+                //height: double.infinity,
+              ), */
+          ),
+          title: Text(
+            peopleSearchDetails[index].picPoster.name,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle: Text(
+            "${500}" + " photos - " + "${500}" + " followers",
+            overflow: TextOverflow.fade,
+          ),
+          trailing: FlatButton(
+            shape: Border.all(
+              color: Colors.black,
+              width: 2,
+            ),
+            color: Colors.transparent,
+            onPressed: () {},
+            child: Text("+" + " Follow"),
+          ),
+        );
+      },
+    );
   }
 }
